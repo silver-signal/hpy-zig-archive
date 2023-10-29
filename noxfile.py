b3f77a4df2bc23350208d1669b8cff0338d8bcf8
february cozzocrea <february.cozzocrea@gmail.com>
2023-10-29 13:52:41 -0700

    python 3.12 added to noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 import nox
 
-python_versions = ["3.9", "3.10", "3.11", "pypy3.8", "pypy3.9"]
+python_versions = ["3.9", "3.10", "3.11", "3.12", "pypy3.8", "pypy3.9"]
 
 @nox.session(python=python_versions)
 def tests(session):
