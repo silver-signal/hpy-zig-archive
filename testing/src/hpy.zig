d56bd762a8d1dce475190cbeb1cc1ba9eacef794
february cozzocrea <february.cozzocrea@gmail.com>
2023-11-01 10:46:39 -0700

    code refactor

--- testing/src/hpy.zig
+++ testing/src/hpy.zig
@@ -1,44 +1,51 @@
 const std = @import("std");
 
 pub const ffi = @import("./ffi.zig");
-pub const HPyContext = ffi.HPyContext;
-pub const HPy = ffi.HPy;
-pub const HPyGlobal = ffi.HPyGlobal;
-pub const HPyUnicode_FromString = ffi.HPyUnicode_FromString;
-pub const cpy_PyObject = ffi.cpy_PyObject;
-pub const cpy_PyMethodDef = ffi.cpy_PyMethodDef;
-pub const _HPyFunc_args_NOARGS = ffi._HPyFunc_args_NOARGS;
-pub const _HPy_CallRealFunctionFromTrampoline = ffi._HPy_CallRealFunctionFromTrampoline;
-pub const HPyFunc_NOARGS = ffi.HPyFunc_NOARGS;
-pub const HPyFunc_Signature = ffi.HPyFunc_Signature;
-pub const HPyCFunction = ffi.HPyCFunction;
-pub const HPyDef = ffi.HPyDef;
-pub const HPyModuleDef = ffi.HPyModuleDef;
-pub const HPyDef_Kind_Meth = ffi.HPyDef_Kind_Meth;
-pub const HPy_ssize_t = ffi.HPy_ssize_t;
-pub const HPyMeth = ffi.HPyMeth;
-pub const cpy_PyCFunction = ffi.cpy_PyCFunction;
-pub const HPY_ABI_VERSION = ffi.HPY_ABI_VERSION;
-pub const HPY_ABI_VERSION_MINOR = ffi.HPY_ABI_VERSION_MINOR;
+pub const ABI_VERSION = ffi.HPY_ABI_VERSION;
+pub const ABI_VERSION_MINOR = ffi.HPY_ABI_VERSION_MINOR;
+pub const ssize_t = ffi.HPy_ssize_t;
 
-pub inline fn HPyZigDef_METH(trampoline_context: *?*HPyContext, meth_name: []const u8, comptime impl: anytype, func_sig: HPyFunc_Signature) HPyDef {
+pub const HPy = ffi.HPy;
+pub const Context = ffi.HPyContext;
+pub const Global = ffi.HPyGlobal;
+pub const Unicode_FromString = ffi.HPyUnicode_FromString;
+
+pub const Func_VARARGS = ffi.HPyFunc_VARARGS;
+pub const Func_KEYWORDS = ffi.HPyFunc_KEYWORDS;
+pub const Func_NOARGS = ffi.HPyFunc_NOARGS;
+pub const Func_O = ffi.HPyFunc_O;
+pub const Func_Signature = ffi.HPyFunc_Signature;
+
+pub const CFunction = ffi.HPyCFunction;
+pub const Def = ffi.HPyDef;
+pub const Def_Kind_Meth = ffi.HPyDef_Kind_Meth;
+pub const Meth = ffi.HPyMeth;
+
+const cpy_PyCFunction = ffi.cpy_PyCFunction;
+const cpy_PyObject = ffi.cpy_PyObject;
+const cpy_PyMethodDef = ffi.cpy_PyMethodDef;
+const _Func_args_NOARGS = ffi._HPyFunc_args_NOARGS;
+const _CallRealFunctionFromTrampoline = ffi._HPy_CallRealFunctionFromTrampoline;
+const _HPyModuleDef = ffi.HPyModuleDef;
+
+pub inline fn Def_METH(trampoline_context: *?*Context, meth_name: []const u8, comptime impl: anytype, func_sig: Func_Signature) Def {
     const S = struct {
         pub fn meth_trampoline(self: ?*cpy_PyObject) callconv(.C) ?*cpy_PyObject {
-            var a: _HPyFunc_args_NOARGS = _HPyFunc_args_NOARGS{
+            var a: _Func_args_NOARGS = _Func_args_NOARGS{
                 .self = self,
                 .result = null,
             };
-            _HPy_CallRealFunctionFromTrampoline(trampoline_context.*, @as(c_uint, @bitCast(func_sig)), @as(HPyCFunction, @ptrCast(@alignCast(&impl))), @as(?*anyopaque, @ptrCast(&a)));
+            _CallRealFunctionFromTrampoline(trampoline_context.*, @as(c_uint, @bitCast(func_sig)), @as(CFunction, @ptrCast(@alignCast(&impl))), @as(?*anyopaque, @ptrCast(&a)));
             return a.result;
         }
     };
 
-    var method_definition: HPyDef = HPyDef{
-        .kind = @as(c_uint, @bitCast(HPyDef_Kind_Meth)),
+    var method_definition: Def = Def{
+        .kind = @as(c_uint, @bitCast(Def_Kind_Meth)),
         .unnamed_0 = .{
-            .meth = HPyMeth{
+            .meth = Meth{
                 .name = @ptrCast(meth_name),
-                .impl = @as(HPyCFunction, @ptrCast(@alignCast(&impl))),
+                .impl = @as(CFunction, @ptrCast(@alignCast(&impl))),
                 .cpy_trampoline = @as(cpy_PyCFunction, @ptrCast(@alignCast(&S.meth_trampoline))),
                 .signature = @as(c_uint, @bitCast(func_sig)),
                 .doc = null,
@@ -49,29 +56,29 @@ pub inline fn HPyZigDef_METH(trampoline_context: *?*HPyContext, meth_name: []con
     return method_definition;
 }
 
-pub inline fn HPyZig_InitGlobalContext(mod_name: []const u8) *?*HPyContext {
+pub inline fn initGlobalContext(mod_name: []const u8) *?*Context {
     // Exports the function used by HPy to pass a context to by used by functions
     const S = struct {
-        pub var _ctx_for_trampolines: ?*HPyContext = null;
-        pub fn HPyInitGlobalContext_module(ctx: ?*HPyContext) callconv(.C) void {
+        pub var _ctx_for_trampolines: ?*Context = null;
+        pub fn InitGlobalContext_module(ctx: ?*Context) callconv(.C) void {
             _ctx_for_trampolines = ctx;
         }
     };
     const hpyinitcontext_name = "HPyInitGlobalContext_" ++ mod_name;
-    @export(S.HPyInitGlobalContext_module, .{ .name = hpyinitcontext_name, .linkage = .Strong });
+    @export(S.InitGlobalContext_module, .{ .name = hpyinitcontext_name, .linkage = .Strong });
 
     return &S._ctx_for_trampolines;
 }
 
-pub const HPyZigModuleDef = extern struct {
+pub const ModuleDef = extern struct {
     doc: [*]const u8 = "",
-    size: HPy_ssize_t = 0,
+    size: ssize_t = 0,
     legacy_methods: ?*cpy_PyMethodDef = null,
-    defines: [*:null]?*HPyDef,
-    globals: ?[*:null]?*HPyGlobal = null,
+    defines: [*:null]?*Def,
+    globals: ?[*:null]?*Global = null,
 };
 
-pub inline fn HPyZig_MODINIT(mod_name: []const u8, module_def: ?*HPyZigModuleDef) void {
+pub inline fn MODINIT(mod_name: []const u8, module_def: ?*ModuleDef) void {
 
     // Exports the functions used by HPy for getting the ABI version
     const major_version_modname = "get_required_hpy_major_version_" ++ mod_name;
@@ -81,19 +88,19 @@ pub inline fn HPyZig_MODINIT(mod_name: []const u8, module_def: ?*HPyZigModuleDef
 
     // Exports the function used by HPy to get the module definition
     const S = struct {
-        const _hpy_module_def = @as(*HPyModuleDef, @ptrCast(@constCast(module_def)));
-        pub fn HPyInit_module() callconv(.C) ?*HPyModuleDef {
-            return _hpy_module_def;
+        const __module_def = @as(*_HPyModuleDef, @ptrCast(@constCast(module_def)));
+        pub fn Init_module() callconv(.C) ?*_HPyModuleDef {
+            return __module_def;
         }
     };
-    const hpyinit_modname = "HPyInit_" ++ mod_name;
-    @export(S.HPyInit_module, .{ .name = hpyinit_modname, .linkage = .Strong });
+    const init_modname = "HPyInit_" ++ mod_name;
+    @export(S.Init_module, .{ .name = init_modname, .linkage = .Strong });
 }
 
 fn get_required_hpy_major_version_module() callconv(.C) u32 {
-    return ffi.HPY_ABI_VERSION;
+    return ABI_VERSION;
 }
 
 fn get_required_hpy_minor_version_module() callconv(.C) u32 {
-    return ffi.HPY_ABI_VERSION_MINOR;
+    return ABI_VERSION_MINOR;
 }
--- testing/src/quickstart.zig
+++ testing/src/quickstart.zig
@@ -2,33 +2,24 @@ const std = @import("std");
 
 const hpy = @import("./hpy.zig");
 const HPy = hpy.HPy;
-const HPyDef = hpy.HPyDef;
-const HPyContext = hpy.HPyContext;
-const HPyFunc_NOARGS = hpy.HPyFunc_NOARGS;
-const HPyUnicode_FromString = hpy.HPyUnicode_FromString;
-const HPyZigModuleDef = hpy.HPyZigModuleDef;
 
-const HPyZigDef_METH = hpy.HPyZigDef_METH;
-const HPyZig_InitGlobalContext = hpy.HPyZig_InitGlobalContext;
-const HPyZig_MODINIT = hpy.HPyZig_MODINIT;
+const module_ctx = hpy.initGlobalContext("quickstart_zig");
 
-const ctx_for_trampolines = HPyZig_InitGlobalContext("quickstart_zig");
-
-pub export var say_hello = HPyZigDef_METH(ctx_for_trampolines, "say_hello", say_hello_impl, HPyFunc_NOARGS);
-pub fn say_hello_impl(ctx: ?*HPyContext, self: HPy) callconv(.C) HPy {
+pub export var say_hello = hpy.Def_METH(module_ctx, "say_hello", say_hello_impl, hpy.Func_NOARGS);
+pub fn say_hello_impl(ctx: ?*hpy.Context, self: HPy) callconv(.C) HPy {
     _ = self;
-    return HPyUnicode_FromString(ctx, "Hello world!");
+    return hpy.Unicode_FromString(ctx, "Hello world!");
 }
 
-var QuickstartZigMethods = [_:null]?*HPyDef{
+var QuickstartZigMethods = [_:null]?*hpy.Def{
     &say_hello,
 };
 
-var quickstart_zig_def = HPyZigModuleDef{
+var quickstart_zig_def = hpy.ModuleDef{
     .doc = "HPy Quickstart Zig Example",
     .defines = &QuickstartZigMethods,
 };
 
 comptime {
-    HPyZig_MODINIT("quickstart_zig", &quickstart_zig_def);
+    hpy.MODINIT("quickstart_zig", &quickstart_zig_def);
 }
